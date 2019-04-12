6c5867d2d52a7ac0de1593bef1a2cce52b10ef82
RomanOst <r.ostrovskiy@digital-sector.ru>
2019-04-12 11:35:43 +0300

    Правка в админке

--- views/manage/index.php
+++ views/manage/index.php
@@ -51,14 +51,6 @@ $this->params['breadcrumbs'][] = $this->title;
                 'value' => function ($model) {
                     return Yii::t('yii2mod.comments', Status::getLabel($model->status));
                 },
-                'filter' => function ($list) {
-                    $arr = [];
-                    foreach ($list as $item) {
-                        $arr = array_merge($arr ,Yii::t('yii2mod.comments', $item));
-                    }
-
-                    return $arr;
-                },
                 'filterInputOptions' => ['prompt' => Yii::t('yii2mod.comments', 'Select Status'), 'class' => 'form-control'],
             ],
             [
